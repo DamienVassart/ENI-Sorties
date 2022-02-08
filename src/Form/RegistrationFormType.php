b5d478394eb777c20bfcc1d5c6bda66a37015010
EugenieFuchs <eugenie.fuchs@yahoo.fr>
2022-02-08 10:26:58 +0100

    page d'inscription participant fonctionne

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -25,15 +25,16 @@ class RegistrationFormType extends AbstractType
             ->add('plainPassword', PasswordType::class, [
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
+                'label' => 'Mot de passe',
                 'mapped' => false,
                 'attr' => ['autocomplete' => 'new-password'],
                 'constraints' => [
                     new NotBlank([
-                        'message' => 'Please enter a password',
+                        'message' => 'Veuillez entrer un mot de passe',
                     ]),
                     new Length([
                         'min' => 6,
-                        'minMessage' => 'Your password should be at least {{ limit }} characters',
+                        'minMessage' => 'Votre mot de passe doit faire au moins {{ limit }} caractères',
                         // max length allowed by Symfony for security reasons
                         'max' => 4096,
                     ]),
