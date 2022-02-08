fbe24fe073bb6ba99b5f7502cb425673fd35d47e
EugenieFuchs <eugenie.fuchs@yahoo.fr>
2022-02-08 11:27:37 +0100

    nav à jour, rôle à refaire au propre

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/admin", name="admin_")
+ */
+class AdminController extends AbstractController
+{
+    /**
+     * @Route("/villes", name="villes")
+     */
+    public function afficherVilles(): Response
+    {
+        return $this->render('admin/index.html.twig', [
+            'controller_name' => 'AdminController',
+        ]);
+    }
+
+    /**
+     * @Route("/campus", name="campus")
+     */
+    public function afficherCampus(): Response
+    {
+        return $this->render('admin/index.html.twig', [
+            'controller_name' => 'AdminController',
+        ]);
+    }
+
+}
--- src/Entity/Participant.php
+++ src/Entity/Participant.php
@@ -121,11 +121,7 @@ class Participant implements UserInterface, PasswordAuthenticatedUserInterface
      */
     public function getRoles(): array
     {
-        $roles = $this->roles;
-        // guarantee every user at least has ROLE_USER
-        $roles[] = 'ROLE_USER';
-
-        return array_unique($roles);
+        return $this->roles;
     }
 
     public function setRoles(array $roles): self
