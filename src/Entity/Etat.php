0a2c2473bb6d5c0b0d73f5a6c0d43d473a40b6c1
DamienVassart <damienvassart@tutanota.com>
2022-02-08 08:57:22 +0100

    ajout des valeurs de libellé pour l'Entité Etat

--- src/Entity/Etat.php
+++ src/Entity/Etat.php
@@ -4,6 +4,7 @@ namespace App\Entity;
 
 use App\Repository\EtatRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass=EtatRepository::class)
@@ -18,6 +19,7 @@ class Etat
     private $id;
 
     /**
+     * @Assert\Choice(choices={"Crée", "Ouverte", "Clôturée", "En cours", "Passée", "Annulée"})
      * @ORM\Column(type="string", length=255)
      */
     private $libelle;
