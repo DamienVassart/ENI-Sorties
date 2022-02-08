b6a019269c6425d6d0f308270b02e2150633360f
EugenieFuchs <eugenie.fuchs@yahoo.fr>
2022-02-08 10:50:05 +0100

    rôle admin lors de 1ère inscription user

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -21,6 +21,7 @@ class RegistrationController extends AbstractController
     public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, UserAuthenticatorInterface $userAuthenticator, AppAuthenticator $authenticator, EntityManagerInterface $entityManager): Response
     {
         $user = new Participant();
+        $user->getRoles('ROLE_ADMIN');
         $form = $this->createForm(RegistrationFormType::class, $user);
         $form->handleRequest($request);
 
