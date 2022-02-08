ec2b91832ce8d8d42ec0641e9f443cd5bf28e79d
DamienVassart <damienvassart@tutanota.com>
2022-02-08 10:04:55 +0100

    suppression du type lors de la déclaration de propriété urlGenerator dans AppAuthenticator

--- src/Security/AppAuthenticator.php
+++ src/Security/AppAuthenticator.php
@@ -21,7 +21,7 @@ class AppAuthenticator extends AbstractLoginFormAuthenticator
 
     public const LOGIN_ROUTE = 'app_login';
 
-    private UrlGeneratorInterface $urlGenerator;
+    private $urlGenerator;
 
     public function __construct(UrlGeneratorInterface $urlGenerator)
     {
