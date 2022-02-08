414718cb2d356975749808ec4cf10252354d63c2
DamienVassart <damienvassart@tutanota.com>
2022-02-08 12:34:31 +0100

    formulaire création de sortie

---
+++ src/Form/SortieType.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Campus;
+use App\Entity\Sortie;
+use App\Repository\CampusRepository;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class SortieType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nom')
+            ->add('dateHeureDebut', DateTimeType::class, [
+                'date_label' => 'Date et Heure de la Sortie: '
+            ])
+            ->add('duree', NumberType::class, [
+                'label' => 'Durée: '
+            ])
+            ->add('dateLimiteInscription', DateType::class, [
+                'html5' => true,
+                'widget' => 'single_text'
+            ])
+            ->add('nbInscriptionsMax', NumberType::class, [
+                'label' => 'Nombre de places'
+            ])
+            ->add('infosSortie')
+            ->add('campus', EntityType::class, [
+                'class' => Campus::class,
+                'choice_label' => 'Campus: ',
+                'query_builder' => function(CampusRepository $campusRepository) {
+                    return $campusRepository->createQueryBuilder('c')->orderBy('c.nom', 'ASC');
+                }
+            ])
+            ->add('ville')
+            ->add('rue')
+            ->add('codePostal')
+            ->add('latitude')
+            ->add('longitude')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Sortie::class,
+        ]);
+    }
+}
