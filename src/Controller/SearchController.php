f15e6b259e1dfe1a8901e9df6f6c40cb8f83215c
LYDIEALT <lydie.allart2021@campus-eni.fr>
2022-02-14 11:28:41 +0100

    search campus test

--- src/Controller/SearchController.php
+++ src/Controller/SearchController.php
@@ -14,22 +14,26 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class SearchController extends AbstractController
 {
-    /**
-     * @Route("/campus/search", name="search_campus")
-     */
-        public function searchCampus(CampusRepository $campusRepository, Request $request, EntityManagerInterface  $entityManager):Response
-        {
-            $campus = new Campus();
-            $searchCampusForm = $this->createForm(CampusType::class, $campus);
-            $searchCampusForm->handleRequest($request);
-
-            $searchCampusForm = $this->createFormBuilder(SearchCampusType::class);
-
-            return $this->render('search/campus.html.twig', [
-                'rechercheCampus' => 'Recherche d un campus',
-                'campus' => $campus,
-                'searchCampusForm' => $searchCampusForm->createView()
-            ]);
+//    /**
+//     * @Route("/campus/search", name="search_campus")
+//     */
+//
+//        public function searchCampus(CampusRepository $campusRepository, Request $request, EntityManagerInterface  $entityManager):Response
+//        {
+//            $campus = new Campus();
+//            $searchCampusForm = $this->createForm(CampusType::class, $campus);
+//            $searchCampusForm->handleRequest($request);
+//
+//            $searchCampusForm = $this->createFormBuilder(SearchCampusType::class);
+//
+//            return $this->render('search/campus.html.twig', [
+//                'rechercheCampus' => 'Recherche d un campus',
+//                'campus' => $campus,
+//                'searchCampusForm' => $searchCampusForm->createView(),
+//            ]);
+//        }
+        public function Search () {
+            return $this-> render('search/campus.html.twig');
         }
 
 }
--- src/Form/SearchCampusType.php
+++ src/Form/SearchCampusType.php
@@ -4,19 +4,21 @@ namespace App\Form;
 
 use App\Entity\Campus;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class SearchCampusType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('nom', CampusType::class, [
-                'choices' => [
-                    array_combine(Campus::nom, Campus::nom)
-                ]
-            ])
-            ->add('recherche', SubmitType::class);
+            ->add('nom');
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $revolver->setDefaults([
+            'data_class' => Campus::class,
+    ]);
     }
 }
